fa9ee0d0058625c789385c58919df4b9863effc1
Robotnik Games <85544501+robotnik-dev@users.noreply.github.com>
2025-07-29 09:54:03 +0200

    Update and rename install-hx.sh to install.sh

--- helix/install.sh
+++ helix/install.sh
@@ -3,3 +3,8 @@ sudo apt update
 sudo add-apt-repository ppa:maveonair/helix-editor
 sudo apt update
 sudo apt install helix
+
+# moving configs
+mkdir -p ~/.config
+cp ~/helix/.config/helix/ ~/.config/
+rm -r ~/helix
