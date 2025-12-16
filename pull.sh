790f6ed95cc81dfe51ed98c0186e525cddf51b50
robotnik-dev <robotnik.games@gmail.com>
2025-12-16 09:54:27 +0100

    add sync scripts

---
+++ pull.sh
@@ -0,0 +1,2 @@
+cp helix/config.toml ~/.config/helix/config.toml
+cp helix/languages.toml ~/.config/helix/languages.toml
---
+++ push.sh
@@ -0,0 +1,2 @@
+cp ~/.config/helix/config.toml helix/config.toml
+cp ~/.config/helix/languages.toml helix/languages.toml
