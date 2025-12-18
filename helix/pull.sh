f2597612b3f8e23760f1661c0729f4dedd716f63
robotnik-dev <robotnik.games@gmail.com>
2025-12-18 16:46:37 +0100

    snippet support

--- helix/pull.sh
+++ helix/pull.sh
@@ -1,2 +1,4 @@
 cp config.toml ~/.config/helix/config.toml
 cp languages.toml ~/.config/helix/languages.toml
+cp snippets/rust.toml ~/.config/helix/snippets/rust.toml
+
--- helix/push.sh
+++ helix/push.sh
@@ -1,2 +1,4 @@
 cp ~/.config/helix/config.toml config.toml
 cp ~/.config/helix/languages.toml languages.toml
+cp ~/.config/helix/snippets/rust.toml snippets/rust.toml
+
