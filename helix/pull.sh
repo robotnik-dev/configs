fcf15bf9f88b4a38d1845d7f0161d9779cd18de8
robotnik-dev <robotnik.games@gmail.com>
2026-05-22 19:46:22 +0200

    Fr 22. Mai 19:46:22 CEST 2026

--- helix/pull.sh
+++
@@ -1,5 +0,0 @@
-cp config.toml ~/.config/helix/config.toml
-cp languages.toml ~/.config/helix/languages.toml
-mkdir -p ~/.config/helix/snippets
-cp snippets/rust.toml ~/.config/helix/snippets/rust.toml
-
--- helix/push.sh
+++
@@ -1,4 +0,0 @@
-cp ~/.config/helix/config.toml config.toml
-cp ~/.config/helix/languages.toml languages.toml
-cp ~/.config/helix/snippets/rust.toml snippets/rust.toml
-
